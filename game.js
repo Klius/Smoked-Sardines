d535190a69fa01534d725f2430313629afb95590
klius <kliusescarlopini@gmail.com>
2016-08-22 23:31:34 +0200

    added sardina to the tuna

--- game.js
+++ game.js
@@ -52,7 +52,7 @@ var imageRepository = new function() {
 	
 	// Set images src
 	this.background.src = "imgs/bg.png";
-	this.spaceship.src = "imgs/ship.png";
+	this.spaceship.src = "imgs/sardina.svg";
 	this.bullet.src = "imgs/bullet.png";
 }
 
@@ -250,14 +250,14 @@ function Ship() {
 	this.bulletPool = new Pool(30);
 	this.bulletPool.init();
 
-	var fireRate = 15;
-	var counter = 0;
+	//var fireRate = 15;
+	//var counter = 0;
 	
 	this.draw = function() {
 		this.context.drawImage(imageRepository.spaceship, this.x, this.y);
 	};
 	this.move = function() {	
-		counter++;
+		//counter++;
 		// Determine if the action is move action
 		if (KEY_STATUS.left || KEY_STATUS.right ||
 			KEY_STATUS.down || KEY_STATUS.up) {
@@ -291,10 +291,6 @@ function Ship() {
 			this.draw();
 		}
 		
-		if (KEY_STATUS.space && counter >= fireRate) {
-			this.fire();
-			counter = 0;
-		}
 	};
 	
 	/*
@@ -325,37 +321,7 @@ function Cannon() {
 	this.move = function() {	
 		counter++;
 		// Determine if the action is move action
-		if (KEY_STATUS.left || KEY_STATUS.right ||
-			KEY_STATUS.down || KEY_STATUS.up) {
-			// The ship moved, so erase it's current image so it can
-			// be redrawn in it's new location
-			this.context.clearRect(this.x, this.y, this.width, this.height);
-			
-			// Update x and y according to the direction to move and
-			// redraw the ship. Change the else if's to if statements
-			// to have diagonal movement.
-			if (KEY_STATUS.left) {
-				this.x -= this.speed
-				if (this.x <= 0) // Keep player within the screen
-					this.x = 0;
-			} else if (KEY_STATUS.right) {
-				this.x += this.speed
-				if (this.x >= this.canvasWidth - this.width)
-					this.x = this.canvasWidth - this.width;
-			} 
-			if (KEY_STATUS.up) {
-				this.y -= this.speed
-				if(this.y <= 0)
-					this.y = 0;
-			} else if (KEY_STATUS.down) {
-				this.y += this.speed
-				if (this.y >= this.canvasHeight - this.height)
-					this.y = this.canvasHeight - this.height;
-			}
-			
-			// Finish by redrawing the ship
-			this.draw();
-		}
+		
 		
 		if (KEY_STATUS.space && counter >= fireRate) {
 			this.fire();
@@ -467,7 +433,12 @@ KEY_CODES = {
   38: 'up',
   39: 'right',
   40: 'down',
-}
+  97: 'bottom_left',
+  98: 'bottom_middle',
+  99: 'bottom_right',
+  100: 'middle_left',
+  102: '',
+};
 
 // Creates the array to hold the KEY_CODES and sets all their values
 // to false. Checking true/flase is the quickest way to check status
