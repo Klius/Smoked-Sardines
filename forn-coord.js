97fcd2f91df09e93f033ef733cb2b21654743b90
Klius <kliusescarlopini@gmail.com>
2016-12-27 21:18:17 +0100

    added more animation frames for fire

--- forn-coord.js
+++ forn-coord.js
@@ -38,5 +38,7 @@ var fornSprite = [
 var fireAngle = [180,0,90,270];
 var fireSprite = [
 					{"sx":0,"sy":0},
-					{"sx":100,"sy":0}
+					{"sx":100,"sy":0},
+					{"sx":200,"sy":0},
+					{"sx":300,"sy":0}
 ];
